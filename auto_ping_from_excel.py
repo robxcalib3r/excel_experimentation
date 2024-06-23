6c48920fc602e55fa4c884ad635a024923bbe000
Robxcalib3r <khisarobin@gmail.com>
2024-06-23 17:17:25 +0600

    feat: pandas excel read and write csv

--- auto_ping_from_excel.py
+++ auto_ping_from_excel.py
@@ -2,18 +2,19 @@ from openpyxl import load_workbook
 import os
 import platform
 import subprocess
+import pandas as pd
 
-def ping_ip(ip):
-    # Option for the number of packers as a function of OS
-    param = '-n' if platform.system().lower() == 'windows' else '-c'
-    command = ['ping', param, '1', ip]
-    # response = os.system(f"ping -c 3 {ip}")
-    return os.system(' '.join(command)) == 0
+# def ping_ip(ip):
+#     # Option for the number of packers as a function of OS
+#     param = '-n' if platform.system().lower() == 'windows' else '-c'
+#     command = ['ping', param, '1', ip]
+#     # response = os.system(f"ping -c 3 {ip}")
+#     return os.system(' '.join(command)) == 0
 
 def ping_ip_subprocess_Exists(ip):
     # Option for the number of packers as a function of OS
     param = '-n' if platform.system().lower() == 'windows' else '-c'
-    command = ['ping', param, '3', ip]
+    command = ['ping', param, '1', ip]
     response = subprocess.run(command, capture_output=True)
     return response.returncode == 0
 
@@ -25,13 +26,29 @@ def modifyExcelByPing(file_path, read_column, write_column):
         ip = sheet.cell(row=row, column=read_column).value
         if ip is not None:
             status = 'online' if ping_ip_subprocess_Exists(ip) else 'offline'
-            print(f'{ip} : {status}')
+            print(f'{row} --- {ip} : {status}')
             sheet.cell(row=row, column=write_column, value=status)
         else:
             status = 'Not valid IP'
             print(status)
             sheet.cell(row=row, column=write_column, value=status)
-    workbook.save(file_path)
+    workbook.save(f'{file_path.split(".")[0]}_mod.{file_path.split(".")[1]}')
+
+def pingXl2csv(file_path, read_column, write_column):
+    df = pd.read_excel(file_path)
+    # print(df.head())
+    for index in range(1, len(df)):
+        ip = str(df.iloc[index, read_column])
+        # print(type(ip))
+        if ip != 'nan':
+            status = 'online' if ping_ip_subprocess_Exists(ip) else 'offline'
+            print(f'{index} --- {ip} : {status}')   
+        else:
+            status = 'Not valid IP'
+            print(status)
+
+        df.iloc[index, write_column] = status
+    df.to_csv(f'{file_path.split(".")[0]}_mod.csv')
 
 if __name__ == "__main__":
-    modifyExcelByPing("all_veam_backup_list_6-22.xlsm", 3, 4)
+    pingXl2csv("all_veam_backup_list_6-22.xlsm", 2, 3)
